03e32e4a72d06c9cc8c347bd30281ab4dd56feb4
Mathius <devachievem@gmail.com>
2021-07-12 12:36:36 -0700

    refactor home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CreatePost from './CreatePost';
+import PostList from './PostList';
+
+const index = () => {
+  return (
+    <div>
+      <CreatePost />
+      <PostList />
+    </div>
+  );
+};
+
+export default index;
