5b758f3c421e1632045de46bbffb5728d7544d93
Mathius <devachievem@gmail.com>
2021-07-12 12:36:28 -0700

    create createpost component

---
+++ src/components/Home/CreatePost.js
@@ -0,0 +1,44 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import React, { useState } from 'react';
+
+const CREATE_POST_MUTATION = gql`
+  mutation ($textBody: String!, $posterId: String!) {
+    createPost(createPostInput: { textBody: $textBody, posterId: $posterId }) {
+      id
+      posterId
+      textBody
+    }
+  }
+`;
+
+const CreatePost = () => {
+  const [postText, setPostText] = useState('');
+  const onContentChanged = (e) => setPostText(e.target.value);
+
+  const authToken = JSON.parse(localStorage.getItem('auth'))
+    ? JSON.parse(localStorage.getItem('auth'))
+    : '';
+  // console.log(JSON.parse(authToken));
+  const [createUser] = useMutation(CREATE_POST_MUTATION, {
+    variables: {
+      textBody: postText,
+      posterId: authToken.user.id,
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+    onCompleted: () => history.push('/'),
+  });
+  console.log(postText);
+  return (
+    <form onSubmit={createUser} className="flex space-x-2">
+      <textarea onChange={onContentChanged} className="rounded-lg"></textarea>
+      <button type="submit" className="btn btn-primary">
+        Post
+      </button>
+    </form>
+  );
+};
+
+export default CreatePost;
