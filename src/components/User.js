ed3520199965221c08a3a1cff013dfcd1fef19f5
Mathius <devachievem@gmail.com>
2021-07-12 12:36:03 -0700

    add rendered posts

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import UserPosts from './UserPosts';
 
 const User = (props) => {
   const { user } = props;
+  const renderedPosts = user.posts.map((post, index) => {
+    return <UserPosts key={index} post={post} />;
+  });
+
   return (
     <div>
       <div>{user.username}</div>
+      <div>{renderedPosts}</div>
     </div>
   );
 };
