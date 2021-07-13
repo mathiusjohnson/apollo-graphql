2053ac2f39fdf1339c9a0f80515db86251971586
Mathius <devachievem@gmail.com>
2021-07-13 10:55:31 -0700

    add edit fn to post list item

--- src/components/Home/PostListItem.js
+++ src/components/Home/PostListItem.js
@@ -1,7 +1,16 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router';
+import useVisualMode from '../../hooks/useVisualMode';
+import EditPostForm from './EditPostForm';
+
+const SHOW = 'SHOW';
+// const CONFIRM = "CONFIRM";
+// const SAVING = "SAVING";
+const EDITING = 'EDITING';
+// const ERROR_SAVE = "ERROR_SAVE";
+// const ERROR_DELETE = "ERROR_DELETE";
 
 const DELETE_POST_MUTATION = gql`
   mutation ($id: String!) {
@@ -11,8 +20,30 @@ const DELETE_POST_MUTATION = gql`
   }
 `;
 
+const UPDATE_POST_MUTATION = gql`
+  mutation ($textBody: String!, $id: String!) {
+    updatePost(updatePostInput: { textBody: $textBody, id: $id }) {
+      id
+      textBody
+    }
+  }
+`;
+
 const PostListItem = ({ post, POSTS_QUERY }) => {
+  const [postText, setPostText] = useState(post.textBody);
+
   const history = useHistory();
+  const { mode, transition } = useVisualMode(SHOW);
+
+  function onEdit() {
+    transition(EDITING);
+  }
+
+  function onSaveEdit() {
+    updatePost();
+    transition(SHOW);
+  }
+
   const loggedInUserId = localStorage.getItem('userId');
   const myPost = loggedInUserId === post.user.id;
 
@@ -25,52 +56,59 @@ const PostListItem = ({ post, POSTS_QUERY }) => {
         id: post.id,
         __typename: 'Post',
       });
-      console.log('normalized: ', normalizedId, removePost);
       cache.evict({ id: normalizedId });
       cache.gc();
     },
-    // cache.modify({
-    //   fields: {
-    //     posts(existingPostsRef, { readField }) {
-    //       console.log('posts? ', existingPostsRef);
-    //       return existingPostsRef.filter(
-    //         (postRef) => post.id !== readField('id', postRef)
-    //       );
-    //     },
-    //   },
-    // });
-    // const { getAllPosts } = cache.readQuery({
-    //   query: POSTS_QUERY,
-    // });
-    // const updatedPosts = getAllPosts.filter((feedPost) => {
-    //   return feedPost.id !== post.id;
-    // });
-
-    // console.log('updated: ', updatedPosts);
-    // cache.writeQuery({
-    //   query: POSTS_QUERY,
-    //   data: {
-    //     getAllPosts: updatedPosts,
-    //   },
-    // });
     onError: (error) => {
       console.log(error);
     },
     onCompleted: () => history.push('/'),
   });
 
+  const [updatePost] = useMutation(UPDATE_POST_MUTATION, {
+    variables: {
+      textBody: postText,
+      id: post.id,
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+    // onCompleted: () => history.push('/'),
+  });
+
   return (
-    <div className="flex space-x-2">
-      <div>{post.textBody}</div>
-      <div className="italic">- {post.user.username}</div>
-      {myPost ? (
-        <button onClick={deletePost} className="btn btn-warning">
-          Delete
-        </button>
-      ) : (
-        ''
-      )}
-    </div>
+    <form onSubmit={updatePost} className="flex space-x-2">
+      <div className="flex space-x-2 my-1">
+        {mode === SHOW && <div>{post.textBody}</div>}
+        {mode === EDITING && (
+          <EditPostForm postText={postText} setPostText={setPostText} />
+        )}
+        <div className="italic">- {post.user.username}</div>
+        {myPost ? (
+          <div className="space-x-2">
+            <button onClick={deletePost} className="btn btn-warning">
+              Delete
+            </button>
+            {mode === SHOW && (
+              <button onClick={onEdit} className="btn btn-warning">
+                Edit
+              </button>
+            )}
+            {mode === EDITING && (
+              <button
+                type="submit"
+                onClick={onSaveEdit}
+                className="btn btn-primary"
+              >
+                Save
+              </button>
+            )}
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+    </form>
   );
 };
 
