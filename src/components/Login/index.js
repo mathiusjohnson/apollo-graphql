724bed8f3c75c21a5c9aa5bc6304bbbbc4288523
Mathius <devachievem@gmail.com>
2021-07-13 10:07:53 -0700

    pass down login fn from index

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Login from './Login';
 
-const index = () => {
+const index = ({ login }) => {
   return (
     <div>
-      <Login />
+      <Login login={login} />
     </div>
   );
 };
