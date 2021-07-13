aa4689c16d695e9fc90b07d14bab896c5f840842
Mathius <devachievem@gmail.com>
2021-07-13 10:08:01 -0700

    refactor login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,52 +1,26 @@
-import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import React, { useState } from 'react';
-import { useHistory } from 'react-router';
-import { AUTH_TOKEN } from '../../constants';
 
-const LOGIN_MUTATION = gql`
-  mutation LoginMutation($username: String!, $password: String!) {
-    login(input: { username: $username, password: $password }) {
-      token
-      user {
-        username
-        id
-      }
-    }
-  }
-`;
-
-const Login = () => {
+const Login = ({ login }) => {
   const [error, setError] = useState('');
-  const history = useHistory();
   const [formState, setFormState] = useState({
     login: true,
     username: '',
     password: '',
   });
 
-  const [login] = useMutation(LOGIN_MUTATION, {
-    variables: {
-      username: formState.username,
-      password: formState.password,
-    },
-    onError: ({ error }) => {
-      setError('Incorrect Login Credentials, please try again');
-      return;
-    },
-    onCompleted: ({ login }) => {
-      const authStorage = {
-        AUTH_TOKEN,
-        token: login.token,
-        user: login.user,
-      };
-      localStorage.setItem('auth', JSON.stringify(authStorage));
-      history.push('/');
-    },
-  });
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    login({
+      variables: {
+        username: formState.username,
+        password: formState.password,
+      },
+    });
+  };
 
   return (
-    <div>
+    <form onSubmit={(e) => handleLoginSubmit(e)}>
       <h4 className="mv3">{formState.login ? 'Login' : 'Sign Up'}</h4>
       <div className="flex flex-column">
         <input
@@ -73,7 +47,7 @@ const Login = () => {
         />
       </div>
       <div className="flex mt3">
-        <button className="pointer mr2 button" onClick={login}>
+        <button className="pointer mr2 button">
           {formState.login ? 'login' : 'create account'}
         </button>
         <button
@@ -91,7 +65,7 @@ const Login = () => {
         </button>
         <div className="text-red-500">{error}</div>
       </div>
-    </div>
+    </form>
   );
 };
 
