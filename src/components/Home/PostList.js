2af10d82550bb96b017e106ddede799437191591
Mathius <devachievem@gmail.com>
2021-07-14 09:49:36 -0700

    add new posts subscription

--- src/components/Home/PostList.js
+++ src/components/Home/PostList.js
@@ -17,15 +17,40 @@ export const POSTS_QUERY = gql`
   }
 `;
 
+const NEW_POSTS_SUBSCRIPTION = gql`
+  subscription {
+    postAdded {
+      id
+      textBody
+    }
+  }
+`;
+
 const PostList = () => {
-  const { data } = useQuery(POSTS_QUERY);
+  const { data, subscribeToMore } = useQuery(POSTS_QUERY, {
+    pollInterval: 500,
+  });
+
   const renderedPosts =
     data &&
     data.getAllPosts.map((post) => {
-      return (
-        <PostListItem key={post.id} post={post} POSTS_QUERY={POSTS_QUERY} />
-      );
+      return <PostListItem key={post.id} post={post} />;
     });
+
+  subscribeToMore({
+    document: NEW_POSTS_SUBSCRIPTION,
+    updateQuery: (prev, { subscriptionData }) => {
+      console.log(subscriptionData);
+      if (!subscriptionData.data) return prev;
+      const postAdded = subscriptionData.data.postAdded;
+      const exists = prev.getAllPosts.find(({ id }) => id === postAdded.id);
+      if (exists) return prev;
+      console.log('in subscription: ', prev.getAllPosts, postAdded);
+      return Object.assign({}, prev, {
+        posts: [postAdded, ...prev.getAllPosts],
+      });
+    },
+  });
   return <div>{renderedPosts}</div>;
 };
 
