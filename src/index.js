8ab2ff6715d37703b455ea500daa1484c228bb5e
Mathius <devachievem@gmail.com>
2021-07-14 09:49:00 -0700

    implement subscription for post added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-
-import './styles/index.css';
-import App from './components/App';
-// 1
 import {
   ApolloProvider,
   ApolloClient,
@@ -15,16 +11,31 @@ import {
   useQuery,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 import { cache } from './cache';
+import App from './components/App';
 import LoginPage from './pages/Login';
 import Header from './components/Header';
+import './styles/index.css';
 
 export const typeDefs = gql`
   extend type Query {
     isLoggedIn: Boolean!
   }
 `;
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:3000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem('token'),
+    },
+  },
+});
 
 // 2
 const httpLink = createHttpLink({
@@ -44,9 +55,18 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 // 3
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache,
   typeDefs,
 });
